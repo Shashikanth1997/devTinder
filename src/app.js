23ae424fe1cdb0564c20ad3d8421bc954dcdcec9
Shashikanth1997 <shashikanthdussa08@gmail.com>
2026-02-14 11:45:39 +0530

    Routing for Post and Get calls

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express")
-const app =  express()
+const app =express()
+app.use("/user",(req,res)=>{
+    res.send("Works for All")
+})
+app.get("/user",(req,res)=>{
+    res.send({firstname:"shashikanth"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data saved to Db")
+})
 app.use("/new",(req,res)=>{
-    res.send("This is my first program")
+    res.send("this is my new Program in express.bless me shivayya")
 })
-app.listen(1800,()=>{
-    console.log("this is running in 1800 server")
-})
+app.listen(1800,()=>{console.log("this is running in 1800 port")})
