b3a2d20e80868b90083e16cd659b68fb9a68d4e5
Shashikanth1997 <shashikanthdussa08@gmail.com>
2026-02-14 14:36:16 +0530

    two arguments in app.use

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const app =express()
-app.use("/user",(req,res)=>{
-    res.send("Works for All")
-})
-app.get("/user",(req,res)=>{
-    res.send({firstname:"shashikanth"})
-})
-app.post("/user",(req,res)=>{
-    res.send("Data saved to Db")
-})
-app.use("/new",(req,res)=>{
-    res.send("this is my new Program in express.bless me shivayya")
-})
+app.use("/user", (req, res,next) => {
+    // res.send("Works for All");
+    next()
+},(req, res,next)=>{
+    res.send("Second function")
+});
+
+// app.get("/user",(req,res)=>{
+//     res.send({firstname:"shashikanth"})
+// })
+// app.post("/user",(req,res)=>{
+//     res.send("Data saved to Db")
+// })
+// app.use("/new",(req,res)=>{
+//     res.send("this is my new Program in express.bless me shivayya")
+// })
 app.listen(1800,()=>{console.log("this is running in 1800 port")})
