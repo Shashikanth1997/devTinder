5322452188b50207a7d98d4e74805da7d66f1f50
Shashikanth1997 <shashikanthdussa08@gmail.com>
2026-02-14 23:53:20 +0530

    Upto now we are adding static data to post call

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 const express = require("express")
+const connectDB = require("./config/database")
+const User=require("./models/user")
 const app =express()
-app.use("/user", (req, res,next) => {
-    // res.send("Works for All");
-    next()
-},(req, res,next)=>{
-    res.send("Second function")
+// 
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "virat",
+    lastName: "kohli",
+    age: 29,
+  });
+
+  try {
+    await user.save();
+    res.send("Added successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
-// app.get("/user",(req,res)=>{
-//     res.send({firstname:"shashikanth"})
-// })
-// app.post("/user",(req,res)=>{
-//     res.send("Data saved to Db")
-// })
-// app.use("/new",(req,res)=>{
-//     res.send("this is my new Program in express.bless me shivayya")
-// })
-app.listen(1800,()=>{console.log("this is running in 1800 port")})
+
+connectDB().then(()=>{
+    console.log("Connect Established")
+    app.listen(1800,()=>{console.log("this is running in 1800 port")})
+}).catch(()=>{
+    console.log("Connect not connected")
+})
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectDB = async() =>{
+
+await mongoose.connect("mongodb+srv://shashikanthdussa08_db_user:o6AjfiQMfC12BbbD@namastenode.asehufv.mongodb.net/devTinder")
+}
+module.exports=connectDB
